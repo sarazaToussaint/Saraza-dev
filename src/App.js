0c00b8c7121c9a12bde7b9194f8b6048727e2c8f
sarazaToussaint <tmsaraza@gmail.com>
2022-10-30 21:42:41 +0200

    Render Routes in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import NavaBar from './NavaBar';
+import About from './components/About';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Project from './components/Project';
 
 function App() {
   return (
     <>
       <NavaBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Project" element={<Project />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
