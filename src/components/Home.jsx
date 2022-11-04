2e2750b430c14220e3b46b577a3527362d83d412
sarazaToussaint <tmsaraza@gmail.com>
2022-11-04 17:21:20 +0200

    Add Icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { VscGithubAlt, VscTwitter } from 'react-icons/vsc';
+import { ImLinkedin2 } from 'react-icons/im';
 import { NavLink as Link } from 'react-router-dom';
 
 const Home = () => (
@@ -8,14 +10,26 @@ const Home = () => (
       <span> SARAZA</span>
     </h1>
     <p>
-      Hello, I&apos;m Toussaint Computer Scientist | Full-stack web developer and open-source
-      enthusiast with a love for clean code and accessible design.
-      Pair-programming, remote work, and knowledge to work to tight deadlines.
-      Fluent in multiple languages, frameworks, and technologies,
-      and capable of ramping up quickly and efficiently.
+      I am a self-motivated and goal orientated individual with a degree
+      in Computer Science from Kigali Independent University.
+      I have worked on a variety of fullstack projects when studied at Microverse and
+      now open to fulltime opportunities. I have a demonstrated history
+      of both leading and working in teams, along with good time management
+      skills. I enjoy learning new skills and am constantly expanding my
+      current abilities.
     </p>
+    <div className="icon-container">
+      <a href="https://www.linkedin.com/in/toussaintsaraza/" target="_blank" rel="noreferrer">
+        <ImLinkedin2 />
+      </a>
+      <a href="https://twitter.com/ToussaintSaraz1" target="_blank" rel="noreferrer">
+        <VscTwitter />
+      </a>
+      <a href="https://github.com/sarazaToussaint" target="_blank" rel="noreferrer">
+        <VscGithubAlt />
+      </a>
+    </div>
     <Link className="buton" to="/About"><button type="button" className="btn btn-primary">Get in touch</button></Link>
-
   </div>
 );
 
