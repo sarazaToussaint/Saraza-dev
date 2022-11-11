3f7adf1c94edd46168fb594faf34d5101e8fdfe8
sarazaToussaint <tmsaraza@gmail.com>
2022-11-11 22:56:37 +0200

    Update the contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Contact = () => (
   <div>
-    <h1 className="contact">Contact</h1>
+    <h1 className="home-title">
+      TOUSSAINT
+      <span> SARAZA</span>
+    </h1>
+    <p>
+      I am a self-motivated and goal orientated individual with a degree
+      in Computer Science from Kigali Independent University.
+      I have worked on a variety of fullstack projects when studied at Microverse and
+      now open to fulltime opportunities. I have a demonstrated history
+      of both leading and working in teams, along with good time management
+      skills. I enjoy learning new skills and am constantly expanding my
+      current abilities.
+    </p>
   </div>
 );
 
