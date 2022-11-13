bf8231e1792748946c585d3ab1664eddb2cce4ca
sarazaToussaint <tmsaraza@gmail.com>
2022-11-13 23:24:15 +0200

    Modify the contact file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,14 +3,7 @@ import React from 'react';
 const Contact = () => (
   <div>
     <p>
-      Hello,
-      I&apos;m Toussaint Saraza
-      I am a self-motivated and goal orientated individual with a degree
-      in Computer Science from Kigali Independent University.
-      I have worked on a variety of fullstack projects when studied at Microverse and
-      now open to fulltime opportunities. I have a demonstrated history
-      of both leading and working in teams, along with good time management
-      skills.
+      Hey there
     </p>
   </div>
 );
