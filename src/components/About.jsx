b736726f2c4a86defff66f7e965bc1b7162a8b9c
sarazaToussaint <tmsaraza@gmail.com>
2022-11-16 15:35:05 +0200

    Add the structure of the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import data from './database/data.json';
 
 const About = () => (
-  <div>
-    <h1 className="about">About</h1>
-  </div>
+  <section className="about-container">
+    <div className="photo">
+      <img className="dev-img" src={data.data.devPhoto} alt={data.data.developer} />
+    </div>
+    <div className="bio">
+      <h1 className="about-title">{data.data.occupation}</h1>
+      <p>
+        Hello,
+        I&apos;m
+        <span className="name">
+          {' '}
+          {data.data.developer}
+          {' '}
+        </span>
+        a self-motivated and goal orientated individual with a degree
+        in Computer Science from Kigali Independent University.
+        I have worked on a variety of fullstack projects when studied at Microverse and
+        now open to fulltime opportunities. I have a demonstrated history
+        of both leading and working in teams, along with good time management
+        skills. I enjoy learning new skills and am constantly expanding my
+        current abilities.
+      </p>
+    </div>
+  </section>
 );
 
 export default About;
