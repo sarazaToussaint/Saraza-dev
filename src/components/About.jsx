58d6e6481a933520f4799d58d4a597171c15b628
sarazaToussaint <tmsaraza@gmail.com>
2022-11-16 17:24:23 +0200

    Delete a paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,9 +21,9 @@ const About = () => (
         in Computer Science from Kigali Independent University.
         I have worked on a variety of fullstack projects when studied at Microverse and
         now open to fulltime opportunities. I have a demonstrated history
-        of both leading and working in teams, along with good time management
-        skills. I enjoy learning new skills and am constantly expanding my
-        current abilities.
+        of both leading and working in teams, along with good time management skills.
+        {/* skills. I enjoy learning new skills and am constantly expanding my
+        current abilities. */}
       </p>
       <Link className="buton" to="/Skills"><button type="button" className="btn btn-primary">See More</button></Link>
     </div>
