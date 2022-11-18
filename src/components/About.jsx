1dd3c7bbcbc3dd311ef28989bbe69a4090a325ff
sarazaToussaint <tmsaraza@gmail.com>
2022-11-18 23:24:09 +0200

    Create a popup modal

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import data from './database/data.json';
 
-const About = () => (
-  <section className="about-container">
-    <div className="photo">
-      <img className="dev-img" src={data.data.devPhoto} alt={data.data.developer} />
-    </div>
-    <div className="bio">
-      <h1 className="about-title">{data.data.occupation}</h1>
-      <p>
-        Hey,
-        I&apos;m
-        <span className="name">
-          {' '}
-          {data.data.developer}
-          {' '}
-        </span>
-        a self-motivated and goal orientated individual with a degree
-        in Computer Science from Kigali Independent University.
-        I have worked on a variety of fullstack projects when studied at Microverse and
-        now open to fulltime opportunities. I have a demonstrated history
-        of both leading and working in teams, along with good time management skills.
-        {/* skills. I enjoy learning new skills and am constantly expanding my
-          current abilities. */}
-      </p>
+const About = () => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <section className="about-container">
+      <div className="photo">
+        <img className="dev-img" src={data.data.devPhoto} alt={data.data.developer} />
+      </div>
+      <div className="bio">
+        <h1 className="about-title">{data.data.occupation}</h1>
+        <p>
+          Hey,
+          I&apos;m
+          <span className="name">
+            {' '}
+            {data.data.developer}
+            {' '}
+          </span>
+          a self-motivated and goal orientated individual with a degree
+          in Computer Science from Kigali Independent University.
+          I have worked on a variety of fullstack projects when studied at Microverse and
+          now open to fulltime opportunities. I have a demonstrated history
+          of both leading and working in teams, along with good time management skills.
+          {/* skills. I enjoy learning new skills and am constantly expanding my
+            current abilities. */}
+        </p>
       <button type="button" className="btn btn-primary">See More</button>
     </div>
   </section>
 );
+};
 
 export default About;
