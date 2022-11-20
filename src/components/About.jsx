5a24b6ea552d949b39a0a8976cbd0d3dbc04ee1f
sarazaToussaint <tmsaraza@gmail.com>
2022-11-20 21:29:51 +0200

    Make some changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,7 +43,7 @@ const About = () => {
       >
         <div>
           <div className="modal-header">
-            <div className="modal-title">Full-Stack Web Developer</div>
+            <div className="modal-title">About me</div>
             <div>
               <span className="close-button" onClick={handleClose} onKeyDown>
                 x
@@ -58,24 +58,6 @@ const About = () => {
             of both leading and working in teams, along with good time management skills.
             a self-motivated and goal orientated individual with a degree
             in Computer Science from Kigali Independent University.
-            I have worked on a variety of fullstack projects when studied at Microverse and
-            now open to fulltime opportunities. I have a demonstrated history
-            of both leading and working in teams, along with good time management skills.
-            a self-motivated and goal orientated individual with a degree
-            in Computer Science from Kigali Independent University.
-            I have worked on a variety of fullstack projects when studied at Microverse and
-            now open to fulltime opportunities. I have a demonstrated history
-            of both leading and working in teams, along with good time management skills.
-            a self-motivated and goal orientated individual with a degree
-            in Computer Science from Kigali Independent University.
-            I have worked on a variety of fullstack projects when studied at Microverse and
-            open to fulltime opportunities. I have a demonstrated history
-            of both leadnow open to fulltime opportunities. I have a demonstrated history
-            of both leading and working in teams, along with good time management skills.
-            a self-motivated and goal orientated individual with a degree
-            in Computer Science from Kigali Independent University.
-            I have worked on a variety of fullstack projects when studied at Microverse and
-            now ing and working in teams, along with good time management skills.
           </div>
           <div className="modal-footer">
             <button type="button" className="secondary-button" onClick={handleClose}>
