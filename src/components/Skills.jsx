b4bc5349c315df4e19a2846032215354d5e4bc2b
sarazaToussaint <tmsaraza@gmail.com>
2022-12-01 23:08:50 +0200

    Make some changes in Carousel

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,7 +14,7 @@ const Skills = () => (
         adaptiveHeight="true"
         cellSpacing={50}
         pauseOnHover="true"
-        autoplayInterval={9000}
+        autoplayInterval={5000}
         slidesToShow={1}
         swiping="true"
         defaultControlsConfig={{
