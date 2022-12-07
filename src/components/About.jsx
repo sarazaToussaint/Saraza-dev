3b637565c174fa5009e0cb981214ceff1036797b
sarazaToussaint <tmsaraza@gmail.com>
2022-12-07 21:08:37 +0200

    Make some changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,53 +41,36 @@ const About = () => {
         onHide={handleClose}
         renderBackdrop={renderBackdrop}
       >
-        <div
-          <div className={styles.modalHeader}>
-            <div className={styles.modalTitle}>About Me</div>
-          <div className="modal-header">
-            <div className="modal-title">About me</div>
-            <div>
-              <span className={styles.closeButton} onClick={handleClose} onKeyDown>
-                x
+
+        <div className={styles.modalHeader}>
+          <div>
+            <span className={styles.closeButton} onClick={handleClose} onKeyDown>
+              x
+            </span>
+          </div>
+        </div>
+        <div className={styles.modalDesc}>
+          <div className="bio">
+            <h1 className="about-title">{data.data.occupation}</h1>
+            <p>
+              Hey,
+              I&apos;m
+              <span className="name">
+                {' '}
+                {data.data.developer}
+                {' '}
               </span>
+              a self-motivated and goal orientated individual with a degree
+              in Computer Science from Kigali Independent University.
+              I have worked on a variety of fullstack projects when studied at Microverse and
+              now open to fulltime opportunities. I have a demonstrated history
+              of both leading and working in teams, along with good time management skills.
+            </p>
+            <div className={styles.resume}>
+              <a href="https://docs.google.com/document/d/1Gz_HMGGmernqwVFcTqWqM7A8Fy9NqvI6YBc1dQSPPWI/edit" target="_blank" rel="noreferrer">Check my Resume</a>
             </div>
           </div>
-          <div className={styles.modalDesc}>
-            <div className="bio">
-              <h1 className="about-title">{data.data.occupation}</h1>
-              <p>
-                Hey,
-                I&apos;m
-                <span className="name">
-                  {' '}
-                  {data.data.developer}
-                  {' '}
-                </span>
-                a self-motivated and goal orientated individual with a degree
-                in Computer Science from Kigali Independent University.
-                I have worked on a variety of fullstack projects when studied at Microverse and
-                now open to fulltime opportunities. I have a demonstrated history
-                of both leading and working in teams, along with good time management skills.
-              </p>
-              <div className={styles.resume}>
-                <a href="https://docs.google.com/document/d/1Gz_HMGGmernqwVFcTqWqM7A8Fy9NqvI6YBc1dQSPPWI/edit" target="_blank" rel="noreferrer">Check my Resume</a>
-              </div>
-            </div>
-            <Skills />
-          <div className="modal-desc">
-            a self-motivated and goal orientated individual with a degree
-            in Computer Science from Kigali Independent University.
-            I have worked on a variety of fullstack projects when studied at Microverse and
-            now open to fulltime opportunities. I have a demonstrated history
-            of both leading and working in teams, along with good time management skills.
-            a self-motivated and goal orientated individual with a degree
-            in Computer Science from Kigali Independent University.
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="secondary-button" onClick={handleClose}>
-              Close
-            </button>
-          </div>
+          <Skills />
         </div>
       </Modal>
     </section>
