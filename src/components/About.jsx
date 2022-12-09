9d516ff31bbf45093f373d8bc0d4a19617f4590a
sarazaToussaint <tmsaraza@gmail.com>
2022-12-09 20:31:50 +0200

    Make some changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,11 +26,11 @@ const About = () => {
             {data.data.developer}
             {' '}
           </span>
-          a self-motivated and goal orientated individual with a degree
+          {/* a self-motivated and goal orientated individual with a degree
           in Computer Science from Kigali Independent University.
           I have worked on a variety of fullstack projects when studied at Microverse and
           now open to fulltime opportunities. I have a demonstrated history
-          of both leading and working in teams, along with good time management skills.
+          of both leading and working in teams, along with good time management skills. */}
         </p>
         <button type="button" onClick={() => setShowModal(true)} className="btn btn-primary">See More</button>
       </div>
