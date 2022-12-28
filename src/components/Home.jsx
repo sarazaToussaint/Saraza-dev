54b399bd9dbe4a1600888d45a74157b563e47c56
sarazaToussaint <tmsaraza@gmail.com>
2022-12-28 23:18:22 +0200

    Make some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => (
           <VscGithubAlt />
         </a>
       </div>
-      <Link className="buton" to="/Contact"><button type="button" className="btn btn-primary">Get in touch</button></Link>
+      <Link className="buton" to="/Contact"><button type="button" className="btn btn-primary">See More</button></Link>
     </div>
   </div>
 );
