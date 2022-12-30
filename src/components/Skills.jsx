7890c94ff48a314caa1f37fc98ddd2f73d56eaee
sarazaToussaint <tmsaraza@gmail.com>
2022-12-30 23:53:27 +0200

    Make some changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -103,7 +103,7 @@ const Skills = () => (
           cellSpacing={50}
           pauseOnHover="true"
           autoplayInterval={8000}
-          slidesToShow={3}
+          slidesToShow={1}
           swiping="true"
           defaultControlsConfig={{
             nextButtonText: null,
