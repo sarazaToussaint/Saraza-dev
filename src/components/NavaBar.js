b2d91b772bc5550d7fc68a76bebb3ac106ea5818
sarazaToussaint <tmsaraza@gmail.com>
2023-01-29 15:36:00 +0200

    Add all the nav links into a div

--- src/components/NavaBar.js
+++ src/components/NavaBar.js
@@ -22,20 +22,22 @@ export default function NavaBar() {
           />
         </svg>
       </button>
-      <ul>
-        <li className="active">
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/About">About</Link>
-        </li>
-        <li>
-          <Link to="/Project">Projects</Link>
-        </li>
-        <li>
-          <Link to="/Contact">Contact</Link>
-        </li>
-      </ul>
+      <div className="nav-links">
+        <ul>
+          <li className="active">
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+          <li>
+            <Link to="/Project">Projects</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
