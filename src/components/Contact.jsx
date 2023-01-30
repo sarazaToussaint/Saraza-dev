c15c30ab3b1caa946f876423852566e77f68e053
sarazaToussaint <tmsaraza@gmail.com>
2023-01-30 10:07:22 +0200

    Make some changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Contact = () => (
-  <div>
+  <>
     <h1 className="contact">
       Not yet implemented
     </h1>
@@ -25,7 +25,7 @@ const Contact = () => (
         </g>
       </svg>
     </div>
-  </div>
+  </>
 );
 
 export default Contact;
