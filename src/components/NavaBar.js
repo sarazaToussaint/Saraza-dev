58331dd5049bd5a39d31de6ab7cfea81ffb6484d
sarazaToussaint <tmsaraza@gmail.com>
2023-02-18 22:11:42 +0200

    Make some changes

--- src/components/NavaBar.js
+++ src/components/NavaBar.js
@@ -7,7 +7,7 @@ export default function NavaBar() {
     <nav className="nav">
       <h1 className="site-title">
         Saraza
-        <span>.de</span>
+        <span>.dev</span>
       </h1>
       <button
         type="button"
