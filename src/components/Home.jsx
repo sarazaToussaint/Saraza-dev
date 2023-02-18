5790b1de1655722511f07e77a658289bd22cd9f1
sarazaToussaint <tmsaraza@gmail.com>
2023-02-18 22:07:51 +0200

    Make some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => (
           <VscGithubAlt />
         </a>
       </div>
-      <Link className="buton" to="/Contact"><button type="button" className="btn btn-primary">Get in touch</button></Link>
+      <Link className="buton" to="/Contact"><button type="butto" className="btn btn-primary">Get in touch</button></Link>
     </div>
 
     <div className="waves-container">
