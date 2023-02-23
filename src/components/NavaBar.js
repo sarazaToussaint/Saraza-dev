b321245341d4e97949fd19c8b162d92ab4e108fe
sarazaToussaint <tmsaraza@gmail.com>
2023-02-23 21:20:06 +0200

    Add keyframes

--- src/components/NavaBar.js
+++ src/components/NavaBar.js
@@ -7,7 +7,7 @@ export default function NavaBar() {
     <nav className="nav">
       <h1 className="site-title">
         Saraza
-        <span>.dev</span>
+        <span className="myDiv">.dev</span>
       </h1>
       <button
         type="button"
