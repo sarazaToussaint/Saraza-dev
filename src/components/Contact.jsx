4cbcd60e1185c66f9904022da889e5a8183c8693
sarazaToussaint <tmsaraza@gmail.com>
2023-02-24 23:21:35 +0200

    Fix some errors

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -120,7 +120,7 @@ function Contact() {
       </div>
       <div className="waves-container">
         <svg
-          className="wave"
+          className="waves"
           xmlns="http://www.w3.org/2000/svg"
           xmlnsXlink="http://www.w3.org/1999/xlink"
           viewBox="0 24 150 28"
