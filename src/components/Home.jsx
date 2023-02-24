d11b2cb31b75c70f1ca117417c7520303028fe2c
sarazaToussaint <tmsaraza@gmail.com>
2023-02-24 23:17:00 +0200

    Make some changes on the animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => (
         <defs>
           <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
         </defs>
-        <g className="parallax">
+        <g className="parall">
           <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
           <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
           <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
