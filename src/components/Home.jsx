93d493843dfd82ac3ace25174ac95e8389316926
sarazaToussaint <tmsaraza@gmail.com>
2023-02-26 23:25:29 +0200

    Fix some errors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => (
         className="waves"
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox="0 24 150 28"
+        viewBox="0 24 150 29"
         preserveAspectRatio="none"
         shapeRendering="auto"
       >
