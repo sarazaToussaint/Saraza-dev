7dc18a0bf4be4f663997417c9595464640d644fa
sarazaToussaint <tmsaraza@gmail.com>
2023-03-14 13:12:26 +0200

    Add the skills to my it own page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import NavaBar from './components/NavaBar';
 import About from './components/About';
+import Skills from './components/Skills';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import Project from './components/Project';
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
+        <Route path="/Skills" element={<Skills />} />
         <Route path="/Project" element={<Project />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
